c66d4692213ddbdbab9e2b4d8f676ee5e1199823
porco <627135316@qq.com>
2015-06-12 21:04:55 +0800

    add jquery

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,11 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by Porco on 2015/5/21 0021.
  */
 @RestController
-public class LoginController {
+public class LoginController{
 
     @RequestMapping("/login")
     public ModelAndView login(){
-        System.out.println("hello Porco!!");
         return new ModelAndView("login");
     }
 }
+
